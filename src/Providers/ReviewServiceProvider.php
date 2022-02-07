5495405eb667c83c26a90cac397a54b2b6f9c27a
jabber <2898117012@qq.com>
2022-02-07 22:12:57 +0800

    init

---
+++ src/Providers/ReviewServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dealskoo\Review\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReviewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}

89159bd76a31bb4d50577421e69401f2cdd664bf
jabber <2898117012@qq.com>
2022-02-21 18:40:45 +0800

    init

---
+++ database/migrations/2022_02_21_100943_create_reviews_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique()->nullable();
+            $table->string('title');
+            $table->string('cover')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('approved')->default(false);
+            $table->boolean('can_comment')->default(true);
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('seller_id');
+            $table->timestamps();
+            $table->foreign('country_id')->references('id')->on('countries')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('seller_id')->references('id')->on('sellers')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}

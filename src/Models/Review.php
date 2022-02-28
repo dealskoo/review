2bded4c68fd7a41c822f1ca8612a6a02c5c0b70e
jabber <2898117012@qq.com>
2022-02-28 20:17:27 +0800

    init

---
+++ src/Models/Review.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dealskoo\Review\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+}

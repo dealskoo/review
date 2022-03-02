2fef35ef2b843b04e3cb8e8cdaf378acecfd3723
jabber <2898117012@qq.com>
2022-03-02 11:26:46 +0800

    init

--- src/Models/Review.php
+++ src/Models/Review.php
@@ -2,10 +2,41 @@
 
 namespace Dealskoo\Review\Models;
 
+use Dealskoo\Comment\Traits\Commentable;
+use Dealskoo\Country\Traits\HasCountry;
+use Dealskoo\Seller\Traits\HasSeller;
+use Dealskoo\Tag\Traits\Taggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    use HasFactory;
+    use HasFactory, HasCountry, HasSeller, Taggable, Commentable;
+
+    protected $appends = [
+        'cover_url'
+    ];
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'cover',
+        'content',
+        'published_at',
+        'approved',
+        'can_comment',
+        'country_id',
+        'seller_id'
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'approved' => 'boolean',
+        'can_comment' => 'boolean',
+    ];
+
+    public function getCoverUrlAttribute()
+    {
+        return empty($this->cover) ? '' : Storage::url($this->cover);
+    }
 }

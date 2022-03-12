972f1e901712f9638a33b35117ea18fd20bf7b05
jabber <2898117012@qq.com>
2022-03-12 10:29:27 +0800

    init

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories\Dealskoo\Review\Models;
+
+use Dealskoo\Country\Models\Country;
+use Dealskoo\Review\Models\Review;
+use Dealskoo\Seller\Models\Seller;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReviewFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Review::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'slug' => $this->faker->unique()->slug,
+            'title' => $this->faker->title,
+            'cover' => $this->faker->imageUrl,
+            'content' => $this->faker->text,
+            'published_at' => $this->faker->dateTime,
+            'approved' => $this->faker->boolean,
+            'can_comment' => $this->faker->boolean,
+            'country_id' => Country::factory()->create(),
+            'seller_id' => Seller::factory()->create(),
+        ];
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dealskoo\Review\Tests;
+
+use Dealskoo\Review\Providers\ReviewServiceProvider;
+
+abstract class TestCase extends \Dealskoo\Seller\Tests\TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            ReviewServiceProvider::class
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
